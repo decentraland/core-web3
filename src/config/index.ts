c65520f06ae00be26b695125e2be0506960064b8
Juanma Hidalgo <juanma06@gmail.com>
2026-02-03 13:22:10 +0100

    feat: export thirdweb connector

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,8 +1,8 @@
 export { ChainId, getChainById, isSupportedChain, productionChains, supportedChains, testChains } from './chains'
 export type { SupportedChainId } from './chains'
 
-export { magic } from './connectors'
-export type { MagicParameters } from './connectors'
+export { magic, thirdweb } from './connectors'
+export type { MagicParameters, ThirdwebParameters } from './connectors'
 
 export { clearWagmiState, createWeb3CoreConfig } from './wagmi'
 export type { Web3CoreConfig, Web3CoreConfigOptions } from './wagmi'
