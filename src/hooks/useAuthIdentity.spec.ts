d5287ad5638184855e2d023679ef90bd28912177
Braian Mellor <braianj@gmail.com>
2026-03-25 12:22:34 -0300

    fix: remove unused act import from useAuthIdentity spec

--- src/hooks/useAuthIdentity.spec.ts
+++ src/hooks/useAuthIdentity.spec.ts
@@ -1,4 +1,4 @@
-import { renderHook, act } from '@testing-library/react'
+import { renderHook } from '@testing-library/react'
 import type { AuthIdentity } from '@dcl/crypto'
 import { useAuthIdentity } from './useAuthIdentity'
 
